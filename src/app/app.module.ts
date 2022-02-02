103b9488f8d3a8057d0a7299561ca7ec7c6d7b01
khangthinhngx <khangthinhngx@gmail.com>
2022-02-02 15:53:24 +0700

    Initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
+import { ItemComponent } from './item/item.component';
+import { ItemFormComponent } from './item/item-form/item-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailFormComponent,
+    ItemComponent,
+    ItemFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/item/item-form/item-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-form',
+  templateUrl: './item-form.component.html',
+  styles: [
+  ]
+})
+export class ItemFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
